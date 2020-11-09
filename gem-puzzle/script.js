98274552e510190e98dcb02475b7e9ee64020621
avbutlov <butloff666@gmail.com>
2020-11-09 15:41:08 +0300

    add files in folder

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -15,9 +15,6 @@ const createElements = () => {
     }
     container.append(cell);
     cell.classList.add("cell");
-    if (code != 0) {
-      cell.setAttribute("draggable", "true");
-    }
     container.classList.add("container");
     wrapper.append(container);
   });
@@ -51,11 +48,6 @@ const dragNDrop = () => {
       }, 0);
     });
 
-
-
-
-    
-
     container.addEventListener("dragend", function () {
       this.classList.remove("empty");
       const emptyContainer = document.querySelector(".empty");
@@ -67,10 +59,70 @@ const dragNDrop = () => {
         currentArr.push(cell.dataset.code);
       });
 
-        
-     
-        console.log(emptyContainer.getBoundingClientRect());
-        console.log(container.getBoundingClientRect());
+      /*
+
+        if (container.getBoundingClientRect().x - 120 == emptyContainer.getBoundingClientRect().x || container.getBoundingClientRect().x + 120 == emptyContainer.getBoundingClientRect().x) {
+         
+         if (container.getBoundingClientRect().y + 120 != emptyContainer.getBoundingClientRect().y && container.getBoundingClientRect().y - 120 != emptyContainer.getBoundingClientRect().y) {
+           if (Math.abs(container.getBoundingClientRect().x - emptyContainer.getBoundingClientRect().x) == 120 && Math.abs(container.getBoundingClientRect().y - emptyContainer.getBoundingClientRect().y) == 0) {
+          console.log('hordraggable')
+           }
+         }
+        }
+
+        if (container.getBoundingClientRect().y - 120 == emptyContainer.getBoundingClientRect().y || container.getBoundingClientRect().y + 120 == emptyContainer.getBoundingClientRect().y) {
+         
+          if (container.getBoundingClientRect().x + 120 != emptyContainer.getBoundingClientRect().x && container.getBoundingClientRect().x - 120 != emptyContainer.getBoundingClientRect().x) {
+            if (Math.abs(container.getBoundingClientRect().y - emptyContainer.getBoundingClientRect().y) == 120 && Math.abs(container.getBoundingClientRect().x - emptyContainer.getBoundingClientRect().x) == 0) {
+           console.log('verdraggable')
+            }
+          }
+         }
+
+
+      if (
+        (container.getBoundingClientRect().x - 120 ==
+          emptyContainer.getBoundingClientRect().x ||
+        container.getBoundingClientRect().x + 120 ==
+          emptyContainer.getBoundingClientRect().x) &&
+          container.getBoundingClientRect().y + 120 !=
+            emptyContainer.getBoundingClientRect().y &&
+          container.getBoundingClientRect().y - 120 !=
+            emptyContainer.getBoundingClientRect().y &&
+          Math.abs(
+            container.getBoundingClientRect().x -
+              emptyContainer.getBoundingClientRect().x
+          ) == 120 &&
+          Math.abs(
+            container.getBoundingClientRect().y -
+              emptyContainer.getBoundingClientRect().y
+          ) == 0
+      ) {
+        console.log("hor");
+      } else if (
+        (container.getBoundingClientRect().y - 120 ==
+          emptyContainer.getBoundingClientRect().y ||
+        container.getBoundingClientRect().y + 120 ==
+          emptyContainer.getBoundingClientRect().y) &&
+          container.getBoundingClientRect().x + 120 !=
+            emptyContainer.getBoundingClientRect().x &&
+          container.getBoundingClientRect().x - 120 !=
+            emptyContainer.getBoundingClientRect().x &&
+          Math.abs(
+            container.getBoundingClientRect().y -
+              emptyContainer.getBoundingClientRect().y
+          ) == 120 &&
+          Math.abs(
+            container.getBoundingClientRect().x -
+              emptyContainer.getBoundingClientRect().x
+          ) == 0
+      ) {
+        console.log('ver')
+      } else {
+        console.log('no')
+      }
+
+*/
     });
 
     container.addEventListener("dragenter", function () {
@@ -116,9 +168,153 @@ const win = () => {
 };
 
 const getNearest = () => {
-  
-}
+  const emptyContainer = document.querySelector(".empty");
+  const containers = document.querySelectorAll(".container");
+
+  containers.forEach((container) => {
+    // поиск горизонтальных ячеек поблизости
+    if (
+      (container.getBoundingClientRect().x - 120 ==
+        emptyContainer.getBoundingClientRect().x ||
+        container.getBoundingClientRect().x + 120 ==
+          emptyContainer.getBoundingClientRect().x) &&
+      container.getBoundingClientRect().y + 120 !=
+        emptyContainer.getBoundingClientRect().y &&
+      container.getBoundingClientRect().y - 120 !=
+        emptyContainer.getBoundingClientRect().y &&
+      Math.abs(
+        container.getBoundingClientRect().x -
+          emptyContainer.getBoundingClientRect().x
+      ) == 120 &&
+      Math.abs(
+        container.getBoundingClientRect().y -
+          emptyContainer.getBoundingClientRect().y
+      ) == 0
+    ) {
+      container.querySelector(".cell").setAttribute("draggable", "true");
+      container.classList.add("container-draggable");
+    } else if (
+      // поиск вертикальных ячеек поблизости
+      (container.getBoundingClientRect().y - 120 ==
+        emptyContainer.getBoundingClientRect().y ||
+        container.getBoundingClientRect().y + 120 ==
+          emptyContainer.getBoundingClientRect().y) &&
+      container.getBoundingClientRect().x + 120 !=
+        emptyContainer.getBoundingClientRect().x &&
+      container.getBoundingClientRect().x - 120 !=
+        emptyContainer.getBoundingClientRect().x &&
+      Math.abs(
+        container.getBoundingClientRect().y -
+          emptyContainer.getBoundingClientRect().y
+      ) == 120 &&
+      Math.abs(
+        container.getBoundingClientRect().x -
+          emptyContainer.getBoundingClientRect().x
+      ) == 0
+    ) {
+      container.querySelector(".cell").setAttribute("draggable", "true");
+      container.classList.add("container-draggable");
+    } else {
+      container.querySelector(".cell").setAttribute("draggable", "false");
+
+      container.classList.remove("container-draggable");
+    }
+  });
+};
+
+const onClickTranslate = () => {
+  const cells = document.querySelectorAll(".cell");
+  const emptyCell = document.querySelector('[data-code="0"]');
+  let emptyContainer = document.querySelector(".empty");
+
+  /*
+
+  containers.forEach((container) => {
+    container.addEventListener("click", function () {
+        if (container.querySelector('.cell').getAttribute("draggable") == "true") {
+      if (
+        this.getBoundingClientRect().x - 120 ==
+        emptyContainer.getBoundingClientRect().x
+      ) {
+        this.querySelector(".cell").style.transform = "translateX(-120%)";
+        console.log(this.querySelector(".cell").getBoundingClientRect());
+        console.log(emptyContainer.getBoundingClientRect());
+      } else if (
+        this.getBoundingClientRect().x + 120 ==
+        emptyContainer.getBoundingClientRect().x
+      ) {
+        this.querySelector(".cell").style.transform = "translateX(120%)";
+      } else if (
+        this.getBoundingClientRect().y - 120 ==
+        emptyContainer.getBoundingClientRect().y
+      ) {
+        this.querySelector(".cell").style.transform = "translateY(-120%)";
+      } else {
+        this.querySelector(".cell").style.transform = "translateY(120%)";
+      }
+    }
+    });
+  });
+
+  */
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", function () {
+      // смотрим только на те контейнеры которые дрэггэбл
+      if (cell.getAttribute("draggable") == "true") {
+        emptyContainer = document.querySelector(".empty");
+        // определяем в какую сторону будем двигать клетку (и все это ради какой-то анимации, ужас)
+        if (
+          this.closest(".container").getBoundingClientRect().x - 120 ==
+          emptyContainer.getBoundingClientRect().x
+        ) {
+          this.style.transform = "translateX(-120%)";
+        } else if (
+          this.closest(".container").getBoundingClientRect().x + 120 ==
+          emptyContainer.getBoundingClientRect().x
+        ) {
+          this.style.transform = "translateX(120%)";
+        } else if (
+          this.closest(".container").getBoundingClientRect().y - 120 ==
+          emptyContainer.getBoundingClientRect().y
+        ) {
+          this.style.transform = "translateY(-120%)";
+        } else {
+          this.style.transform = "translateY(120%)";
+        }
+        emptyCell.remove();
+        // убираем с нынешней пустой клетки класс обычной пустой клетки и добавляем класс временной пустой клетки (вот это я придумал, конечно)
+        emptyContainer.classList.remove("empty");
+        emptyContainer.classList.add("empty-temporal");
+        // на ту клетку с которой двигалась ячейка по клику вешаем класс обычной пустой клетки
+        cell.closest(".container").classList.add("empty");
+      }
+    });
+
+    cell.addEventListener("transitionend", function () {
+        // заново определяем пустую клетку, потому что ее значение ведь изменилось при клике, теперь она там где раньше была ячейка
+      const emptyContainer = document.querySelector(".empty");
+      const emptyTemporal = document.querySelector(".empty-temporal");
+      // добавляем в нашу бывшую пустую клетку передвигаемую ячейку
+      emptyTemporal.appendChild(cell);
+      emptyContainer.append(emptyCell);
+      // это чтобы ячейка не убегала никуда
+      cell.style.transform = "translateY(0)";
+      // снимаем с той клетки на которую передвинули ячейку класс временно пустой, она ведь больше не пустая
+      emptyTemporal.classList.remove("empty-temporal");
+    });
+  });
+};
 
 createElements();
 dragNDrop();
 win();
+onClickTranslate();
+window.addEventListener("load", getNearest);
+window.addEventListener("dragend", getNearest);
+
+// здесь вешаем слушатель именно на клетки из-за того, что транзишн есть еще во время дрэг эгд дропа и он висит на контейнерах и из-за этого могут некоторые проблемы возникать
+let cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+  cell.addEventListener("transitionend", getNearest);
+});
